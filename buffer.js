600f8397c2a87221dc7fc8a71a636ca7df61c4ed
Mikey <michael.williams@enspiral.com>
2017-04-01 16:29:03 +1300

    fix "ReferenceError: Collect is not defined"
    also remove unnecessary `pull` require

--- buffer.js
+++ buffer.js
@@ -1,8 +1,7 @@
-var pull = require('pull-stream/pull')
 var collect = require('pull-stream/sinks/collect')
 
 module.exports = function (cb) {
-  return Collect(function (err, ary) {
+  return collect(function (err, ary) {
     if(err) cb(err)
     else    cb(null, Buffer.concat(ary))
   })
